11c48589cf27a8d6d91fc5e5f94bc24a9d4b0450
jorgevfx <jorgeant20_@outlook.com>
2022-04-27 19:49:10 -0500

    ni

---
+++ common/js/setHeader.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('.header');
+const navstyle = document.getElementById('header');
+
+window.addEventListener('scroll',() => {
+    const offset = window.pageYOffset;
+    if(offset>75){
+        nav.classList.add('scroll');
+        navstyle.style.padding = '15px 15px 15px 15px';
+    } else {
+        nav.classList.remove('scroll');
+    }
+})
+
