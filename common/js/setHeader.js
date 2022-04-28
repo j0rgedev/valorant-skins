b6a4508eeea58cd30b9322e42502453319a2c9b8
jorgevfx <jorgeant20_@outlook.com>
2022-04-27 22:35:47 -0500

    header fix

--- common/js/setHeader.js
+++ common/js/setHeader.js
@@ -2,7 +2,7 @@ const nav = document.querySelector('.header');
 const navstyle = document.getElementById('header');
 
 window.addEventListener('scroll',() => {
-    const offset = window.pageYOffset;
+    const offset = window.scrollY;
     if(offset>75){
         nav.classList.add('scroll');
         navstyle.style.padding = '15px 15px 15px 15px';
